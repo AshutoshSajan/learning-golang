5c143f043460dd1db692a6a4fbdb074285af70a0
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2021-11-18 22:24:59 +0530

    minor change

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -2,7 +2,7 @@ package greetings
 
 import "fmt"
 
-func Hello(name string) string{
+func Hello(name string) (string, error){
 	// Return a greeting that embeds the name in a message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
