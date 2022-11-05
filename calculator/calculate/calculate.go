6ab626295f4463dfff708808517ef649c9a4da77
AshutoshSajan <44765540+AshutoshSajan@users.noreply.github.com>
2022-11-05 20:53:57 +0530

    add calculator module and package and used it

--- calculator/calculate/calculate.go
+++ calculator/calculate/calculate.go
@@ -1,11 +1,11 @@
-package main
+package calculate
 
 import (
 	"fmt"
 	"strconv"
 )
 
-func calculate(num1 int, num2 int, operand string) int {
+func Calculate(num1 int, num2 int, operand string) int {
 	switch operand {
 	case "+":
 		return num1 + num2
@@ -25,7 +25,7 @@ func calculate(num1 int, num2 int, operand string) int {
 	}
 }
 
-func stringToNumber(str string) (n int, e error) {
+func StringToNumber(str string) (n int, e error) {
 	integer, err := strconv.Atoi(str)
 
 	if err != nil {
@@ -36,24 +36,24 @@ func stringToNumber(str string) (n int, e error) {
 	return integer, nil
 }
 
-func getNumberFromInput(text string) int {
+func GetNumberFromInput(text string) int {
 	var number string
 	var err error
 
 	fmt.Printf("> Enter the %v number:", text)
-	_, err = fmt.Scanln(&number)
+	_, _ = fmt.Scanln(&number)
 
-	num, err := stringToNumber(number)
+	num, err := StringToNumber(number)
 
 	if err != nil {
 		fmt.Println("Invalid number")
-		return getNumberFromInput(text)
+		return GetNumberFromInput(text)
 	}
 
 	return num
 }
 
-func getOperand() string {
+func GetOperand() string {
 	var operand string
 	var err error
 
@@ -62,26 +62,22 @@ func getOperand() string {
 
 	if operand != "+" && operand != "-" && operand != "/" && operand != "*" {
 		fmt.Println("Operand must be +, -, * or /")
-		return getOperand()
+		return GetOperand()
 	}
 
 	if err != nil {
 		fmt.Println("Something went wrong", err)
-		return getOperand()
+		return GetOperand()
 	}
 
 	return operand
 }
 
-func calculator() {
-	num1 := getNumberFromInput("first")
-	num2 := getNumberFromInput("second")
-	operand := getOperand()
+func Calculator() {
+	num1 := GetNumberFromInput("first")
+	num2 := GetNumberFromInput("second")
+	operand := GetOperand()
 
-	result := calculate(num1, num2, operand)
+	result := Calculate(num1, num2, operand)
 	fmt.Printf("> %v %v %v is equals to %v\n", num1, operand, num2, result)
-}
-
-func main() {
-	calculator()
 }
